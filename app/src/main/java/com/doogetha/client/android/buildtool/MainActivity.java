30187c5fa4f931fd0f4b6593d263ee6e412e88d4
liese <liese@d7406fb1-6878-4198-8eeb-e43e52bed576>
2015-02-04 11:27:31 +0000

    context menu, drawer items

--- app/src/main/java/com/doogetha/client/android/buildtool/MainActivity.java
+++ app/src/main/java/com/doogetha/client/android/buildtool/MainActivity.java
@@ -1,10 +1,15 @@
 package com.doogetha.client.android.buildtool;
 
 import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.view.ContextMenu;
 import android.view.LayoutInflater;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,6 +20,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.wincor.bcon.framework.android.util.AsyncUITask;
+import com.wincor.bcon.framework.android.util.RestResourceAccessor;
 
 import org.json.JSONArray;
 import org.json.JSONObject;
@@ -26,17 +32,24 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
     private ArrayAdapter<JSONObject> data = null;
 
     /** Holds the list view UI element */
-    private ListView listView = null;
+    private ListView mMainList = null;
+    private ListView mDrawerList = null;
+    private DrawerLayout mDrawerLayout = null;
 
     /** Inner class used for asynchronous loading of data */
     private DataLoader dataLoader = null;
+
+    private CharSequence mTitle;
+    private ActionBarDrawerToggle mDrawerToggle;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        this.listView = (ListView) findViewById(R.id.jobList);
+        // MAIN LIST VIEW
+
+        this.mMainList = (ListView) findViewById(R.id.jobList);
         this.data = new ArrayAdapter<JSONObject>(this, R.layout.main_list_item) {
 
             @Override
@@ -58,14 +71,70 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
             }
         };
 
-        this.listView.setAdapter(this.data);
-        this.listView.setOnItemClickListener(this);
+        this.mMainList.setAdapter(this.data);
+        this.mMainList.setOnItemClickListener(this);
+        registerForContextMenu(this.mMainList);
 
         this.dataLoader = new DataLoader();
+
+        // APPLICATION DRAWER
+
+        mTitle = getTitle();
+        mDrawerLayout = (DrawerLayout) findViewById(R.id.drawer_layout);
+        mDrawerToggle = new ActionBarDrawerToggle(this, mDrawerLayout,
+                R.string.drawer_open, R.string.drawer_close) {
+
+            /** Called when a drawer has settled in a completely closed state. */
+            public void onDrawerClosed(View view) {
+                super.onDrawerClosed(view);
+                getSupportActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+
+            /** Called when a drawer has settled in a completely open state. */
+            public void onDrawerOpened(View drawerView) {
+                super.onDrawerOpened(drawerView);
+                getSupportActionBar().setTitle(mTitle);
+                invalidateOptionsMenu(); // creates call to onPrepareOptionsMenu()
+            }
+        };
+
+        // activate the drawer toggle icon
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        // Set the drawer toggle as the DrawerListener
+        mDrawerLayout.setDrawerListener(mDrawerToggle);
+
+        // APPLICATION DRAWER ITEMS
+
+        mDrawerList = (ListView) findViewById(R.id.left_drawer);
+
+        String[] items = getResources().getStringArray(R.array.drawer_items);
+        // Set the adapter for the list view
+        mDrawerList.setAdapter(new ArrayAdapter<String>(this, R.layout.drawer_list_item, items));
+        // Set the list's click listener
+        mDrawerList.setOnItemClickListener(new ListView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView parent, View view, int position, long id) {
+                new TaskStarter(getResources().getStringArray(R.array.drawer_items)[position], position).go(getString(R.string.loading));
+            }
+        });
 
         refresh();
     }
 
+    @Override
+    protected void onPostCreate(Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        // Sync the toggle state after onRestoreInstanceState has occurred.
+        mDrawerToggle.syncState();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+        mDrawerToggle.onConfigurationChanged(newConfig);
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -76,6 +145,10 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
+
+        // handle drawer toggle icon event:
+        if (mDrawerToggle.onOptionsItemSelected(item)) return true;
+
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
@@ -97,6 +170,25 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
         //editItem(data.getItem(position));
     }
 
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v, ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.context_menu_main, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        switch (item.getItemId()) {
+            case R.id.action_delete:
+                new TaskDeleter(data.getItem(info.position).optString("name")).go(getString(R.string.loading));
+                return true;
+            default:
+                return super.onContextItemSelected(item);
+        }
+    }
     /**
      * Asynchronously refreshes the list data
      */
@@ -122,6 +214,74 @@ public class MainActivity extends ActionBarActivity implements AdapterView.OnIte
         public void doneOk(JSONArray result) {
             data.clear();
             for (int i=0; i<result.length(); i++) data.add(result.optJSONObject(i));
+        }
+
+        @Override
+        public void doneFail(Throwable throwable) {
+            throwable.printStackTrace();
+            Toast.makeText(getApplicationContext(), throwable.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Settings of pending tasks
+     */
+    protected class TaskStarter extends AsyncUITask<JSONObject>
+    {
+        private int position;
+        private String task;
+
+        public TaskStarter(String task, int position) {
+            super(MainActivity.this);
+            this.task = task;
+            this.position = position;
+        }
+
+        @Override
+        public JSONObject doTask() throws Throwable
+        {
+            RestResourceAccessor acc = ((Application)getApplication()).getRestAccessor();
+            acc.getWebRequest().setParam("set", "pending");
+            return acc.getItem(task.replace(" ", "%20"));
+        }
+
+        @Override
+        public void doneOk(JSONObject result) {
+            mDrawerList.setItemChecked(position, false);
+            mDrawerLayout.closeDrawer(mDrawerList);
+            refresh();
+        }
+
+        @Override
+        public void doneFail(Throwable throwable) {
+            throwable.printStackTrace();
+            Toast.makeText(getApplicationContext(), throwable.toString(), Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    /**
+     * Settings of pending tasks
+     */
+    protected class TaskDeleter extends AsyncUITask<String>
+    {
+        private String task;
+
+        public TaskDeleter(String task) {
+            super(MainActivity.this);
+            this.task = task;
+        }
+
+        @Override
+        public String doTask() throws Throwable
+        {
+            RestResourceAccessor acc = ((Application)getApplication()).getRestAccessor();
+            acc.deleteItem(task.replace(" ", "%20"));
+            return "Deleted";
+        }
+
+        @Override
+        public void doneOk(String result) {
+            refresh();
         }
 
         @Override
