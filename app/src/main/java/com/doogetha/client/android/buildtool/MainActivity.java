fda9dfc710c69673fded052dd2c3030b6ae99d81
Thorsten <thorsten@potpiejimmy.de>
2016-08-21 21:34:53 +0200

    some UI fixes

--- app/src/main/java/com/doogetha/client/android/buildtool/MainActivity.java
+++ app/src/main/java/com/doogetha/client/android/buildtool/MainActivity.java
@@ -66,7 +66,7 @@ public class MainActivity extends AppCompatActivity
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        getSupportActionBar().setTitle("Current Jobs");
+        getSupportActionBar().setTitle("PC/E Build Tool");
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
